628e0fedd3768f27527bb005456a518c3824272e
Oliveira, Lucas Alves de <olalvezluc@gmail.com>
2022-09-08 21:30:31 -0300

    criando Promisse

--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Promotion } from '../shared/promotion';
 import { PROMOTIONS } from '../shared/promotions';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,24 +13,15 @@ export class PromotionService {
   constructor() { }
 
   getPromotions(): Promise<Promotion[]> {
-    //return Promise.resolve(PROMOTIONS);
-    return new Promise(resolve=> {
-      setTimeout(() => resolve(PROMOTIONS), 2000);
-    });
+    return of(PROMOTIONS).pipe(delay(2000)).toPromise();
   }
 
   getPromotion(id: string): Promise<Promotion> {
-    //return Promise.resolve(PROMOTIONS.filter((promotion) => (promotion.id === id))[0]);
-    return new Promise(resolve=> {
-      setTimeout(() => resolve(PROMOTIONS.filter((promotion) => (promotion.id === id))[0]), 2000);
-    });
+    return of(PROMOTIONS.filter((promotion) => (promotion.id === id))[0]).pipe(delay(2000)).toPromise();
   }
 
   getFeaturedPromotion(): Promise<Promotion> {
-    //return Promise.resolve(PROMOTIONS.filter((promotion) => promotion.featured)[0]);
-    return  new Promise(resolve=> {
-      setTimeout(() => resolve(PROMOTIONS.filter((promotion) => promotion.featured)[0]), 2000);
-    });
+    return of(PROMOTIONS.filter((promotion) => promotion.featured)[0]).pipe(delay(2000)).toPromise();
   }
 
 }
